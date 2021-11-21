92d1fc0a32bbaee47bff2a7e6bac3130992e4df4
mplaczek99 <mplaczek99@gmail.com>
2021-11-21 16:24:58 -0600

    Added BuildingLists

--- BuildingLists/src/lists/MyArrayList.java
+++ BuildingLists/src/lists/MyArrayList.java
@@ -1,72 +1,153 @@
 package lists;
 
 /**
- * @author <Your Name Here>
+ * @author Michael Placzek
  */
 public class MyArrayList implements SimplifiedList
 {
-	@Override
-	public int size() 
-	{
-		return 0;
+	private Object[] storage;
+	private int size;
+
+	public MyArrayList() {
+		// Instantiates storage
+		storage = new Object[10];
+		size = 0;
 	}
 
 	@Override
-	public boolean isEmpty() 
-	{
+	public int size() {
+		return size;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	@Override
+	public boolean add(Object e) {
+		// Special case where storage is full
+		if (size == storage.length) {
+			storage = new Object[size + 10];
+		}
+
+		// Adds e to storage
+		storage[size] = e;
+
+		// Increment size
+		size++;
+
 		return true;
 	}
 
 	@Override
-	public boolean add(Object e) 
-	{
+	public void clear() {
+		// Creates and sets storage to be a new Object[]
+		storage = new Object[10];
+
+		// Sets size to be 0
+		size = 0;
+	}
+
+	@Override
+	public Object get(int index) {
+		if (index < 0 || index >= size) {
+			// returns null if the index is too small or too big
+			return null;
+		}
+		return storage[index];
+	}
+
+	@Override
+	public Object set(int index, Object element) {
+		if (index < 0 || index >= size) {
+			return null;
+		}
+
+		// Trickery to replace storage's index AND keep it in memory
+		Object temp = storage[index];
+		storage[index] = element;
+
+		// ... and return it
+		return temp;
+	}
+
+	@Override
+	public Object remove(int index) {
+		// Special cases where either index is too big or too small
+		if (index < 0 || index >= size) {
+			return null;
+		}
+
+		// Trickery to keep pop in memory even after storage changes
+		Object pop = storage[index];
+
+		// Loop through storage from index to size
+		// Shift contents down
+		System.arraycopy(storage, index + 1, storage, index, size - index);
+
+		// Decrement size
+		size--;
+		return pop;
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		// Loops through storage
+		for (int i = 0; i < size; i ++) {
+			// return true if storage[index] is the same as o
+			if (storage[i] == o) {
+				return true;
+			}
+		}
 		return false;
 	}
 
 	@Override
-	public void clear() 
-	{
-	}
-
-	@Override
-	public Object get(int index) 
-	{
-		return null;
-	}
-
-	@Override
-	public Object set(int index, Object element) 
-	{
-		return null;
-	}
-
-	@Override
-	public Object remove(int index) 
-	{
-		return null;
-	}
-
-	@Override
-	public boolean contains(Object o) 
-	{
-		return false;
-	}
-
-	@Override
-	public int indexOf(Object o) 
-	{
+	public int indexOf(Object o) {
+		// Loops through storage
+		for (int i = 0; i < size; i ++) {
+			// returns i if storage[i] = o
+			if (storage[i] == o) {
+				return i;
+			}
+		}
 		return -1;
 	}
 
 	@Override
-	public int lastIndexOf(Object o) 
-	{
+	public int lastIndexOf(Object o) {
+		// Loops through the list backwards
+		for (int i = size - 1; i >= 0; i --) {
+			if (storage[i] == o) {
+				// returns i if storage has o
+				return i;
+			}
+		}
 		return -1;
 	}
 
 	@Override
-	public boolean remove(Object o) 
-	{
-		return false;
+	public boolean remove(Object o) {
+		if (size < 0) {
+			return false;
+		}
+
+		int index = indexOf(o);
+
+		// Special case where it did not find the object in storage
+		if (index == -1) {
+			return false;
+		}
+
+		// Loop through storage from 0 to size
+		// Shift contents down
+		if (size - index >= 0) System.arraycopy(storage, index + 1, storage, index, size - index);
+		// IntelliJ (my IDE) recommended I use System.arrayCopy() instead of a for loop
+		// I have no idea how System.arrayCopy() works... I'll have to look into it
+
+		// Decrement size
+		size--;
+		return true;
 	}
 }
--- BuildingLists/src/lists/MyLinkedList.java
+++ BuildingLists/src/lists/MyLinkedList.java
@@ -1,72 +1,224 @@
 package lists;
 
 /**
- * @author <Your Name Here>
+ * @author Michael Placzek
  */
-public class MyLinkedList implements SimplifiedList
-{
-	@Override
-	public int size() 
-	{
-		return 0;
-	}
+public class MyLinkedList implements SimplifiedList {
+    Node head; // make private
+    Node tail;
+    int size;
 
-	@Override
-	public boolean isEmpty() 
-	{
-		return true;
-	}
+    public class Node { // make static?
+        Node next; // make protected?
+        Node previous;
+        Object data;
 
-	@Override
-	public boolean add(Object e) 
-	{
-		return false;
-	}
+        public Node(Object o) {
+            next = null;
+            previous = null;
+            data = o;
+        }
 
-	@Override
-	public void clear() 
-	{
-	}
+        public Node getNext() {
+            return next;
+        }
 
-	@Override
-	public Object get(int index) 
-	{
-		return null;
-	}
+        public void setNext(Node next) {
+            this.next = next;
+        }
 
-	@Override
-	public Object set(int index, Object element) 
-	{
-		return null;
-	}
+        public Node getPrevious() {
+            return previous;
+        }
 
-	@Override
-	public Object remove(int index) 
-	{
-		return null;
-	}
+        public void setPrevious(Node previous) {
+            this.previous = previous;
+        }
 
-	@Override
-	public boolean contains(Object o) 
-	{
-		return false;
-	}
+        public Object getData() {
+            return data;
+        }
 
-	@Override
-	public int indexOf(Object o) 
-	{
-		return -1;
-	}
+        public void setData(Object data) {
+            this.data = data;
+        }
+    }
 
-	@Override
-	public int lastIndexOf(Object o) 
-	{
-		return -1;
-	}
+    public MyLinkedList() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
 
-	@Override
-	public boolean remove(Object o) 
-	{
-		return false;
-	}
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean add(Object e) {
+        Node newNode = new Node(e);
+
+        // Special case where head is null
+        if (head == null) {
+            head = newNode;
+        } else {
+            // Sets the newNode's previous to be the (previous) tail
+            newNode.setPrevious(tail);
+
+            // Sets the next node in the list to be newNode
+            tail.setNext(newNode);
+        }
+        // The newNode is to be the new tail
+        tail = newNode;
+        // Increments the size
+        size++;
+
+        return true;
+    }
+
+    @Override
+    public void clear() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    @Override
+    public Object get(int index) {
+        if (indexSpecialCase(index)) {
+            return null;
+        }
+
+        Node current = head;
+
+        // Makes the next Node to be current
+        for (int i = 0; i < index; i++) {
+            if (current != null) {
+                current = current.getNext();
+            }
+        }
+        assert current != null; // IntelliJ complains if this line is not here
+        return current.getData();
+    }
+
+    @Override
+    public Object set(int index, Object element) {
+        if (indexSpecialCase(index)) {
+            return null;
+        }
+
+        Node current = head;
+
+        for (int i = 0; i < index; i++) {
+            current = current.getNext();
+        }
+
+        Node pop = new Node(current.getData());
+        current.setData(element);
+
+        return pop.getData();
+    }
+
+    @Override
+    public Object remove(int index) {
+        // Check if index is in bounds
+        if (indexSpecialCase(index)) {
+            return null;
+        }
+
+        // Check if special case
+        if (index == 0) {
+            Node temp = head;
+
+            head = head.getNext();
+            temp.setNext(null);
+            size--;
+            return temp.getData();
+        } else { // index > 0
+            Node previous = head;
+
+            for (int count = 1; count < index; count++) {
+                previous = previous.getNext();
+            }
+
+            Node current = previous.getNext();
+
+            previous.setNext(current.getNext());
+            current.setNext(null);
+            size--;
+            return current.getData();
+        }
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        Node current = head;
+
+        while (current != null) {
+            if (current.getData() == o) {
+                return true;
+            }
+            current = current.getNext();
+        }
+        return false;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        Node current = head;
+
+        for (int i = 0; i < size; i++) {
+            if (current.getData() == o) {
+                return i;
+            }
+            current = current.getNext();
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        Node current = tail;
+
+        for (int i = size - 1; i >= 0; i--) {
+            if (current.getData() == o) {
+                return i;
+            }
+            current = current.getPrevious();
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        if (!contains(o)) {
+            return false;
+        }
+
+        Node current = head;
+
+        // Loop index number of times
+        while (current != null) {
+            if (current.getData() == o) {
+                current.setPrevious(current.getNext());
+            }
+            current = current.getNext();
+        }
+
+        // Decrement Size
+        size--;
+
+        return true;
+    }
+
+    private boolean indexSpecialCase(int i) {
+        // Special case where i is too small or too big
+        return i < 0 || i >= size;
+    }
 }
